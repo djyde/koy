525ab7c4d60103c103a8df5bf288ccfa0106ea0b
Randy <randypriv@gmail.com>
2018-11-01 14:41:55 +0800

    init

---
+++ README.md
@@ -0,0 +1,17 @@
+Ey is a markdown previewer based on [carlo](https://github.com/GoogleChromeLabs/carlo).
+
+## Install
+
+```
+$ npm i ey -g
+```
+
+## Usage
+
+```
+$ ey README.md
+```
+
+# License
+
+MIT License
---
+++ bin/ey.js
@@ -0,0 +1,16 @@
+#!/usr/bin/env node
+
+const carlo = require('carlo')
+const path = require('path')
+const filePath = process.argv[2]
+const {parse} = require('../lib/ey')
+
+;(async () => {
+  if (filePath) {
+    const app = await carlo.launch()
+    app.serveFolder(path.resolve(__dirname, '../www'))
+    await app.exposeFunction('parse', _ => parse(filePath))
+    await app.load('index.html')
+  }
+})()
+
