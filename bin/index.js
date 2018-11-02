d953a7f36dd3c23520d23762f9b7a78e7b2595da
Randy <randypriv@gmail.com>
2018-11-02 14:06:31 +0800

    change userDataDir

--- bin/index.js
+++ bin/index.js
@@ -4,11 +4,17 @@ const carlo = require('carlo')
 const path = require('path')
 const filePath = process.argv[2]
 const {parse} = require('../lib')
+const os = require('os')
+
+const tmpDir = path.resolve(os.tmpdir(), '.koy')
 
 ;(async () => {
   if (filePath) {
-    const app = await carlo.launch()
+    const app = await carlo.launch({
+      userDataDir: tmpDir
+    })
     app.serveFolder(path.resolve(__dirname, '../www'))
+    app.on('exit', () => process.exit())
     await app.exposeFunction('parse', _ => {
       return {
         content: parse(filePath),
