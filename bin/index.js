fce196de3890f6e86b3c2ad113f3386f4f7bf8ea
EGOIST <0x142857@gmail.com>
2018-11-02 15:01:32 +0800

    a bunch of new features

--- bin/index.js
+++ bin/index.js
@@ -1,20 +1,28 @@
 #!/usr/bin/env node
 
-const carlo = require('carlo')
+const { EventEmitter } = require('events')
+const fs = require('fs')
 const path = require('path')
-const filePath = process.argv[2]
+const carlo = require('carlo')
+const chokidar = require('chokidar')
 const {parse} = require('../lib')
 const os = require('os')
 
 const tmpDir = path.resolve(os.tmpdir(), '.koy')
 
-;(async () => {
-  if (filePath) {
+const main = async () => {
+  const filePath = path.resolve(process.argv[2] || 'README.md')
+
+  if (filePath && fs.existsSync(filePath)) {
     const app = await carlo.launch({
       userDataDir: tmpDir
     })
     app.serveFolder(path.resolve(__dirname, '../www'))
     app.on('exit', () => process.exit())
+    // Use the native EventEmitter will throw error when calling `event.on`
+    class Events extends EventEmitter {}
+    const event = new Events()
+    await app.exposeObject('event', event)
     await app.exposeFunction('parse', _ => {
       return {
         content: parse(filePath),
@@ -22,6 +30,14 @@ const tmpDir = path.resolve(os.tmpdir(), '.koy')
       }
     })
     await app.load('index.html')
-  }
-})()
 
+    chokidar.watch(filePath, {
+      ignoreInitial: true
+    })
+      .on('change', () => {
+        event.emit('update')
+      })
+  }
+}
+
+main().catch(console.error)
